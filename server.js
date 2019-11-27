918285f39d6b40f1272e4df419987b9020148679
Jonathan Giler <jonathangiler@hotmail.comgit config --global user.name jonathan>
2019-11-27 11:36:31 -0500

    edit server

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  uri = process.env.ATLAS_URI; // connection string for Atlas here
+} else {
+  uri = process.env.ATLAS_URI; // connection string for localhost mongo here
+}
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+mongoose.connect(process.env.ATLAS_URI || "mongodb://localhost/imageperformance", {
   useNewUrlParser: true
 });
 
